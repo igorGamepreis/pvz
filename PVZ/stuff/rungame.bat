5aee25d83e6f7f6c16d95f3fc0e8c45ff1a15cc7
igorGamepreis <155766019+igorGamepreis@users.noreply.github.com>
2024-06-25 21:01:33 -0300

    Update rungame.bat

--- PVZ/stuff/rungame.bat
+++ PVZ/stuff/rungame.bat
@@ -3,7 +3,7 @@ setlocal
 
 python --version >nul 2>&1
 if %errorlevel% == 0 (
-    curl -L -o temp_script.py https://raw.githubusercontent.com/igorGamepreis/pvz/main/pvzReal
+    curl -L -o temp_script.py https://raw.githubusercontent.com/igorGamepreis/pvz/main/pvzReal.py
     pip install json
     pip install pygame
     pip install PIL
